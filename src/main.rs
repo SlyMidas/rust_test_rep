b418c46284e9981a76a2a52dfaf4db9b610cedb5
SlyMidas <112784653+SlyMidas@users.noreply.github.com>
2023-08-22 14:16:41 -0600

    Finished the calculator

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use std::io;
+use std::io
 
 fn main() {
     println!("Guess the number!\n");
     println!("Enter a number: ")
-
-    let mut guess = String::new()
-
-    io::stdin().readline(&mut guess).expect("Failed to read line.");
-    println!("You guessed: {guess}!")
 }
