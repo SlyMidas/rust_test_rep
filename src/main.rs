8a6b11e4cce085bf3f548f290e9873dc94783c34
slyers04 <112784653+SlyMidas@users.noreply.github.com>
2023-08-22 14:30:22 -0600

    Finished the calculator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
-use std::io
+use std::io;
 
 fn main() {
-    println!("Guess the number!\n");
-    println!("Enter a number: ")
+    let (n0, n1);
+    n0 = get_f();
+    n1 =  get_f();
+    let operator = get_c();
+
+    println!("{n0} {operator} {n1} = {}", calc(operator, n0, n1))
+}
+
+fn calc(c: char, n0: f32, n1: f32) -> f32 {
+    let mut n: f32 = 0.0; 
+    if c=='+' {
+        n = n0 + n1;
+    } else if c=='-' {
+        n = n0 - n1;
+    } else if c=='*' {
+        n = n0 * n1;
+    } else if c=='/' {
+        n = n0 / n1;
+    } else if c=='%' {
+        n = n0 % n1;
+    };
+    return n;
+}
+
+fn get_f() -> f32 {
+    println!("Enter a number: ");
+    let mut n: String = String::new();
+    io::stdin().read_line(&mut n).expect("Failed to read line.");
+    return n.trim().parse::<f32>().unwrap();
+}
+
+fn get_c() -> char {
+    println!("Enter an operator: ");
+    let mut c: String = String::new();
+    io::stdin().read_line(&mut c).expect("Failed to read line.");
+    return c.trim().parse::<char>().unwrap();
 }
