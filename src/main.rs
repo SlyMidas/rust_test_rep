81a8c9a45ae5294e4747f8a2099f36652b25507e
SlyMidas <112784653+SlyMidas@users.noreply.github.com>
2023-08-22 13:00:14 -0600

    x

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!\n");
+    println!("Enter a number: ")
+
+    let mut guess = String::new()
+
+    io::stdin().readline(&mut guess).expect("Failed to read line.");
+    println!("You guessed: {guess}!")
 }
